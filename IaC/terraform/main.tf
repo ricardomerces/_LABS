d224ae4c5eef15ddccf80dee83b475d0869351be
rmerces <ricardo.merces@gmail.com>
2023-04-17 10:00:31 -0300

    .

--- IaC/terraform/main.tf
+++ IaC/terraform/main.tf
@@ -2,9 +2,11 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "m1"
+  name = "lab"
 
-  ami                    = "ami-00c39f71452c08778" # Amazon Linux 2023
+  # ami                    = "ami-00c39f71452c08778" # Amazon Linux 2023
+  ami                    = "ami-007855ac798b5175e" #Ubuntu 22.04
+
   instance_type          = "t2.micro"
   key_name               = "aula"
 #   monitoring             = true
